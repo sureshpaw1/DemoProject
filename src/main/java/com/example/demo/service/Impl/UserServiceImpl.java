30e18fb0505a5687a47388ccd1257eccbbf22f38
sureshpaw1 <pawarsuresh1981@gmail>
2024-06-15 22:16:23 +0530

    commit changes in serviceImpl

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -6,39 +6,74 @@ import com.example.demo.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 public class UserServiceImpl implements UserServiceI {
 
-@Autowired
-private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-     // IOC -  Inversion of control which manages Spring bean lifecyle
+    // IOC -  Inversion of control which manages Spring bean lifecyle
 
     // DI- Dependency Injection-Setter ,field & Constructor Dependency Injection
 
 
     @Override
     public User createUser(User user) {
-        return null;
+        User saveduser = userRepository.save(user);
+        return saveduser;
     }
 
     @Override
     public User updateUser(User user, Long userId) {
-        return null;
+
+        User user1 = userRepository.findById(userId).get();
+        user1.setUserName(user.getUserName());
+        user1.setUserAge(user.getUserAge());
+        user1.setAbout(user1.getAbout());
+
+        User updateUser = userRepository.save(user1);
+
+        return updateUser;
     }
 
+    // 1st Suresh
     @Override
-    public User getSingleUser(Long userId) {
+    public User getSingleUser(Long userId) throws Exception {
+
+        userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resource not found on server"));
         return null;
+
+
     }
+     /*   Optional<User> User = userRepository.findById(userId);
+
+        Optional<User> user= userRepository.findById(userId);
+        if(user.isPresent()) {
+            return user.get();
+        }else{
+            throw new Exception("Resource not found on server"+ userId);
+        }
+
+        return null;
+    }*/
+
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        List<User> allusers = userRepository.findAll();
+
+
+        return allusers;
     }
+
 
     @Override
     public void deleteUser(Long userId) {
 
+        User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException("Resource not found on server" + userId));
+
+        userRepository.delete(user);
     }
 }
